292ca709db6437c797aff5d5338f26b3e6d1d661
g0dgar1cc <ting@s.sfusd.edu>
2023-03-03 15:23:21 -0800

    added in an enemy and code for the enemy

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float speed = 3.0f;
+    private GameObject focalPoint;
+    public float speed = 1.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
@@ -18,6 +21,6 @@ public class PlayerControls : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
 
-        playerRb.AddForce(Vector3.forward * forwardInput * speed);
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
     }
 }
